65165e7a044ee82c6230318839760fa7b75a9f65
cjgarcia12 <christian.jgarcia03@gmail.com>
2025-05-04 20:01:55 -0400

    Initialize user controller with basic setup and import statements

---
+++ src/controllers/user.ts
@@ -0,0 +1,6 @@
+import User from "../models/User";
+import { Router } from "express";
+
+const router = Router()
+
+// we can create methods here to export that can be getters and setters and what not
